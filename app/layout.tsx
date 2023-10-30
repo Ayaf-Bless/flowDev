b8eb8ffb5bcf3279d200514b77cd1522f5f1448c
Ayaf-Bless <blessambel1@gmail.com>
2023-10-30 23:41:25 +0300

    add assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import {Inter, Space_Grotesk} from "next/font/google"
 import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "FlowDev",
+  description: "A web site that's there to create the help needed for you all",
+  icons: {
+    icon:"/assets/images/site-logo.svg"
+  }
+}
 
 export default function RootLayout({
   children,
