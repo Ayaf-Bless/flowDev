8649cc9600df99e7b8bc61012842c6bb8ae6797e
Ayaf-Bless <blessambel1@gmail.com>
2023-11-01 12:26:17 +0300

    implement the theme

--- components/shared/navBar/NavBar.tsx
+++ components/shared/navBar/NavBar.tsx
@@ -1,9 +1,12 @@
-import { SignIn, UserButton } from "@clerk/nextjs";
+"use client";
+import { SignIn, UserButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
 
 function NavBar() {
+  const user = useUser();
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href={"/"} className="flex items-center gap-1">
@@ -20,18 +23,21 @@ function NavBar() {
       </Link>
       GlobalSearch
       <div className="flex-between gap-5">
-        Theme
-        <SignIn>
-          <UserButton
-            afterSignOutUrl="/"
-            appearance={{
-              elements: { avatarBox: "h-10 w-10" },
-              variables: {
-                colorPrimary: "#ff7000",
-              },
-            }}
-          />
-        </SignIn>
+        <Theme />
+        <div>
+          <SignIn />
+          {user ? (
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                elements: { avatarBox: "h-10 w-10" },
+                variables: {
+                  colorPrimary: "#ff7000",
+                },
+              }}
+            />
+          ) : null}
+        </div>
       </div>
     </nav>
   );
